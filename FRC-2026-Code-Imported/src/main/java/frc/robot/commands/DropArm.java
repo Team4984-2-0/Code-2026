f2225374810631c762c96ae4a0aa6c1c31f3bb5a
jaustinlong-lgtm <jaustinlong@gmail.com>
2026-02-20 16:53:19 -0800

    Added Commands

---
+++ FRC-2026-Code-Imported/src/main/java/frc/robot/commands/DropArm.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class DropArm extends Command {
+    private Intake intakesub;
+    public DropArm(Intake intakesub){
+            this.intakesub = intakesub;
+            addRequirements(intakesub);
+    }
+    @Override
+        public void execute(){
+            intakesub.IntakeDrop();
+        }
+        @Override
+        public void end(boolean interrupted){
+            intakesub.IntakeDropStop();
+        }
+}
