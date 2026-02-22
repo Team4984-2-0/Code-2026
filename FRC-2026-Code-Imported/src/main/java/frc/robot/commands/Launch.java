cd6cf93b9e8c69cb95d82724322bdc9cb57e070d
KM6DFE <km6dfe@gmail.com>
2026-02-21 19:09:27 -0800

    Added Jeremy's code
    modified launch code

--- FRC-2026-Code-Imported/src/main/java/frc/robot/commands/Launch.java
+++ FRC-2026-Code-Imported/src/main/java/frc/robot/commands/Launch.java
@@ -1,13 +1,13 @@
 package frc.robot.commands;
-
-/** Runs the launcher outward at a fixed speed while held. */
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Launcher;
 
 public class Launch extends Command {
         private Launcher LauncherSubsystem;
+        private static int ShooterCounter = 0;
+        private static int StartHopperRunCount = 30;
+        private static int StartFeedRunCount = 30;
         
-        /** Stores reference and declares subsystem requirement. */
         public Launch(Launcher LauncherSubsystem){
             this.LauncherSubsystem = LauncherSubsystem;
             addRequirements(LauncherSubsystem);
@@ -15,11 +15,15 @@ public class Launch extends Command {
         @Override
         public void execute(){
             LauncherSubsystem.ShootRun(-0.20);
-            LauncherSubsystem.HopperRun(-0.20);
-            LauncherSubsystem.FeedRun(-0.20);
+            ShooterCounter++;
+            if (ShooterCounter > StartHopperRunCount) // 1 second
+              LauncherSubsystem.HopperRun(-0.20);
+            if (ShooterCounter > StartFeedRunCount)  //1.5 seconds
+                LauncherSubsystem.FeedRun(-0.20);
         }
         @Override
         public void end(boolean interrupted){
             LauncherSubsystem.Killswitch();
         }
+        
 }
